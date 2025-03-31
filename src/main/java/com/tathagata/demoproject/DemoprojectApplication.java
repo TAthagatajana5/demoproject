29227b6aa776042009e2afdae721653620b95d81
Tathagata <tathagatajana5@gmail.com>
2025-03-31 16:36:42 +0530

    Add greeting message to main application

--- src/main/java/com/tathagata/demoproject/DemoprojectApplication.java
+++ src/main/java/com/tathagata/demoproject/DemoprojectApplication.java
@@ -8,6 +8,7 @@ public class DemoprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoprojectApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
